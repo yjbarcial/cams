70dbb9f172d4537593997fc942ee36aa8aaa20f6
yjbarcial <yjbarcial@gmail.com>
2025-09-04 15:43:24 +0800

    added loginview in router

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import ArchiveView from '@/views/ArchiveView.vue'
+import LoginView from '@/views/LoginView.vue'
+import ContributorDashboard from '@/views/ContributorDashboard.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +10,16 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: ArchiveView,
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginView,
+    },
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: ContributorDashboard,
     },
   ],
 })
