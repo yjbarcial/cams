cff91c784c7928c72240853337b0b0d07d00a9ca
lhclavel <lovellclavel14@gmail.com>
2025-09-12 16:01:01 +0800

    feat: added extra footer above the main footer

--- src/views/system/ContributorDashboard.vue
+++ src/views/system/ContributorDashboard.vue
@@ -1,6 +1,16 @@
 <script setup>
 import MainHeader from '@/components/layout/MainHeader.vue'
 import Footer from '@/components/layout/Footer.vue'
+import { createVuetify } from 'vuetify'
+
+const vuetify = createVuetify({
+  components: {
+    defaults: {
+      global: true,
+      // This enables auto-importing of all Vuetify components
+    },
+  },
+})
 </script>
 
 <template>
@@ -33,6 +43,82 @@ import Footer from '@/components/layout/Footer.vue'
         </RouterLink>
       </div>
     </section>
+
+    <v-sheet class="contact-info" color="white" width="100%">
+      <v-divider class="mx-5 mt-6" thickness="2" color="black"></v-divider>
+
+      <v-container class="px-12">
+        <!-- Contact Us Header -->
+        <v-row justify="center">
+          <v-col cols="12" class="text-center">
+            <v-card-title class="text-h">Contact Us!</v-card-title>
+          </v-col>
+        </v-row>
+
+        <!-- Content Row -->
+        <v-row justify="space-between" align="start" no-gutters>
+          <v-col cols="12" sm="4" class="d-flex flex-column align-start">
+            <img src="../../assets/images/csu-logo.png" alt="CSU Logo" class="csu-logo mb-4" />
+            <v-card-title class="px-0">Old Administration Bldg.</v-card-title>
+            <v-card-text class="px-0">Caraga State University - Main Campus, 8600</v-card-text>
+          </v-col>
+
+          <v-col cols="12" sm="4">
+            <v-list class="social-links">
+              <v-list-item
+                href="https://facebook.com/thegoldpanicles"
+                target="_blank"
+                rel="noopener"
+                class="justify-start"
+              >
+                <template v-slot:prepend>
+                  <v-icon>mdi-facebook</v-icon>
+                </template>
+                <span>facebook.com/thegoldpanicles</span>
+              </v-list-item>
+
+              <v-list-item
+                href="https://instagram.com/thegoldpanicles"
+                target="_blank"
+                rel="noopener"
+                class="justify-start"
+              >
+                <template v-slot:prepend>
+                  <v-icon>mdi-instagram</v-icon>
+                </template>
+                <span>@thegoldpanicles</span>
+              </v-list-item>
+
+              <v-list-item
+                href="https://issuu.com/thegoldpanicles"
+                target="_blank"
+                rel="noopener"
+                class="justify-start"
+              >
+                <template v-slot:prepend>
+                  <v-icon>mdi-newspaper</v-icon>
+                </template>
+                <span>issuu.com/thegoldpanicles</span>
+              </v-list-item>
+            </v-list>
+          </v-col>
+
+          <v-col cols="12" sm="4" class="d-flex flex-column align-end pe-16">
+            <v-list-item
+              href="mailto:thegoldpanicles@carsu.edu.ph"
+              target="_blank"
+              rel="noopener"
+              class="justify-start px-0"
+            >
+              <template v-slot:prepend>
+                <v-icon>mdi-gmail</v-icon>
+              </template>
+              <span>thegoldpanicles@carsu.edu.ph</span>
+            </v-list-item>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-sheet>
 
     <Footer />
   </div>
@@ -91,6 +177,7 @@ import Footer from '@/components/layout/Footer.vue'
   justify-content: center;
   box-shadow: 0 6px 10px rgba(0, 0, 0, 0.12);
   cursor: pointer;
+  text-decoration: none; /* Add this line to remove underline */
 }
 
 .pill.secondary {
@@ -101,7 +188,44 @@ import Footer from '@/components/layout/Footer.vue'
   font-size: 20px;
 }
 
+.csu-logo {
+  width: 120px;
+  height: auto;
+  object-fit: contain;
+}
+
 /* info section removed */
+
+:deep(.text-h5) {
+  font-size: 1.5rem;
+  font-weight: 600;
+  color: #2f2f2f;
+}
+
+:deep(.v-container) {
+  max-width: 1400px !important;
+}
+
+:deep(.v-card-title) {
+  line-height: 1.2;
+}
+
+:deep(.v-card-text) {
+  padding-top: 8px;
+}
+
+.social-links {
+  padding: 0;
+}
+
+:deep(.social-links .v-list-item) {
+  padding-inline-start: 0;
+  padding-inline-end: 0;
+}
+
+:deep(.social-links .v-list-item-content) {
+  justify-content: flex-start;
+}
 
 @media (max-width: 640px) {
   .title {
